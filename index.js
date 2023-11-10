fbde53b3bd9436977a9294539e4a93f60fbbc54c
wintea <nodomatyi@gmail.com>
2023-11-11 00:03:00 +0100

    finish stores, add logo, complete login screen

--- index.js
+++ index.js
@@ -6,8 +6,14 @@ function CreateAlert(type, message) {
 
 function CreateEnty(text, checked, date) {
     return `<li class="list-group-item ${
-        checked && "checked"
+        checked && console.log("checked")
     }" id="todo-item"><button class="btn btn-danger btn-close" id="btn-todo"aria-label="Close" role="button"></button>${text}</li>`;
+}
+
+function DeleteItem(array, value) {
+    return array.filter((element) => {
+        return element != value;
+    });
 }
 
 function CreateAlertWithRedirect(type, message, redirect) {
@@ -16,3 +22,7 @@ function CreateAlertWithRedirect(type, message, redirect) {
         window.location.href = redirect;
     }, 1500);
 }
+
+// for (let index = 0; index < localStorage.length; index++) {
+//     console.log(localStorage.key(index));
+// }
