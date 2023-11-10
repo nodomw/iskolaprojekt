27ad171bfe54abbe12e4ab76ba1e9fb1235a0f3e
wintea <nodomatyi@gmail.com>
2023-11-10 22:12:44 +0100

    todo saving progress

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ function CreateAlert(type, message) {
         `<div class="alert alert-${type} alert-dismissible fade show" role="alert">${message}<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>`
     );
 }
+
+function CreateEnty(text, checked, date) {
+    return `<li class="list-group-item ${
+        checked && "checked"
+    }" id="todo-item"><button class="btn btn-danger btn-close" id="btn-todo"aria-label="Close" role="button"></button>${text}</li>`;
+}
+
+function CreateAlertWithRedirect(type, message, redirect) {
+    CreateAlert(type, message);
+    setTimeout(() => {
+        window.location.href = redirect;
+    }, 1500);
+}
